92ed1d1d9e9871246f69d6ca432d38a7363bcd1f
Roman Kiblitskyi <roman_kiblitskyi@epam.com>
2024-04-18 13:30:27 +0300

    Add products receiving

---
+++ fa-products-service/src/mockedData/products.ts
@@ -0,0 +1,23 @@
+import Product from '../interfaces/Product'
+
+const mockedProductList: Product[] = [
+    {
+        id: 'id-000',
+        title: 'Rubber duck 1',
+        description: 'This is simple rubber toy 1',
+        price: 5
+    },
+    {
+        id: 'id-001',
+        title: 'Rubber duck 2',
+        description: 'This is simple rubber toy 2',
+        price: 5
+    },
+    {
+        id: 'id-002',
+        title: 'Rubber duck 3',
+        description: 'This is simple rubber toy 3',
+        price: 5
+    }
+]
+export default mockedProductList
